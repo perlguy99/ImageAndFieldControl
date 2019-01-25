a63d79ff40aa3d96a427f0c3be4bea0df350e63f
Brent Michalski <Brent.Michalski@gmail.com>
2019-01-25 08:28:33 -0600

    Initial Commit

---
+++ ImageAndFieldControl/ImageAndFieldControl.h
@@ -0,0 +1,19 @@
+//
+//  ImageAndFieldControl.h
+//  ImageAndFieldControl
+//
+//  Created by Brent Michalski on 1/25/19.
+//  Copyright © 2019 Perlguy, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageAndFieldControl.
+FOUNDATION_EXPORT double ImageAndFieldControlVersionNumber;
+
+//! Project version string for ImageAndFieldControl.
+FOUNDATION_EXPORT const unsigned char ImageAndFieldControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageAndFieldControl/PublicHeader.h>
+
+
